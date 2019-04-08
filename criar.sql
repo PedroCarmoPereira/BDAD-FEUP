0e8d5764bbcf827adf9d5171b5827b88b2c015bd
Simawatt <simaoxo@hotmail.com>
2019-04-08 12:12:58 +0100

    Add files via upload

--- criar.sql
+++ criar.sql
@@ -1,32 +1,22 @@
 PRAGMA foreign_keys = on;
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS AreaTrabalho;
-DROP TABLE IF EXISTS Sala;
 DROP TABLE IF EXISTS Gabinete;
 DROP TABLE IF EXISTS Curso;
 DROP TABLE IF EXISTS Estudante;
 DROP TABLE IF EXISTS Prof;
 DROP TABLE IF EXISTS Staff;
-DROP TABLE IF EXISTS StaffAdminSala;
-DROP TABLE IF EXISTS AreaCientifica;
-DROP TABLE IF EXISTS SubArea;
-DROP TABLE IF EXISTS ProfArea;
 DROP TABLE IF EXISTS StaffDoCurso;
-DROP TABLE IF EXISTS CursoArea;
 DROP TABLE IF EXISTS Leciona;
 DROP TABLE IF EXISTS Classificacao;
 DROP TABLE IF EXISTS Lab;
-DROP TABLE IF EXISTS LabArea;
 DROP TABLE IF EXISTS ProfAssocLab;
 DROP TABLE IF EXISTS ComissaoExecutiva;
 DROP TABLE IF EXISTS ComissaoCientifica;
 DROP TABLE IF EXISTS ComissaoAcompanhamento;
 DROP TABLE IF EXISTS Nucleo;
 DROP TABLE IF EXISTS EstudanteNucleo;
-DROP TABLE IF EXISTS Evento;
-DROP TABLE IF EXISTS ProfOrganiza;
-DROP TABLE IF EXISTS NucleoOrganiza;
+
 
 CREATE TABLE Staff (
 	staffID		INTEGER PRIMARY KEY,
@@ -34,31 +24,12 @@ CREATE TABLE Staff (
 	numTele		INTEGER,
 	dataNasc	DATE NOT NULL,
 	morada		TEXT NOT NULL,
-	areaTrab	TEXT NOT NULL,
 	nif 		INTEGER UNIQUE,
-	--localTrab 	INTEGER NOT NULL,
 	FOREIGN KEY (areaTrab) REFERENCES AreaTrabalho(nome)
-	--FOREIGN KEY (localTrab) REFERENCES Sala(numero)
-);
-
-CREATE TABLE AreaTrabalho (
-	nome TEXT PRIMARY KEY
-);
-
-CREATE TABLE StaffAdminSala (
-	salaID		INTEGER PRIMARY KEY,
-	staffID		INTEGER,
-	FOREIGN KEY (staffID) REFERENCES Staff(staffID),
-	FOREIGN KEY (salaID) REFERENCES Sala(numero)
-);
-
-CREATE TABLE Sala (
-	numero INTEGER PRIMARY KEY,
-	numPCs INTEGER
 );
 
 CREATE TABLE Gabinete (
-	numero  INTEGER PRIMARY KEY
+	numero  Text PRIMARY KEY
 );
 
 CREATE TABLE Curso (
@@ -97,33 +68,6 @@ CREATE TABLE Prof (
 	FOREIGN KEY (numGabin) REFERENCES Gabinete(numero)
 );
 
-CREATE TABLE AreaCientifica (
-	areaID		INTEGER PRIMARY KEY,
-	nome 		TEXT
-);
-
-CREATE TABLE SubArea (
-	subID	INTEGER,
-	supID	INTEGER,
-	PRIMARY KEY (subID, supID),
-	FOREIGN KEY (subID) REFERENCES AreaCientifica,
-	FOREIGN KEY (supID) REFERENCES AreaCientifica
-);
-
-CREATE TABLE ProfArea (
-	profID 	INTEGER,
-	areaID	INTEGER,
-	PRIMARY KEY (profID, areaID),
-	FOREIGN KEY (profID) REFERENCES Prof(profID),
-	FOREIGN KEY (areaID) REFERENCES AreaCientifica(areaID)
-);
-
-CREATE TABLE CursoArea (
-	codigo 	INTEGER	PRIMARY KEY,
-	areaID	INTEGER,
-	FOREIGN KEY (codigo) REFERENCES Curso(codigo),
-	FOREIGN KEY (areaID) REFERENCES AreaCientifica(areaID)
-);
 
 CREATE TABLE Leciona  (
 	profID INTEGER,
@@ -147,14 +91,6 @@ CREATE TABLE Lab (
 	profID INTEGER,
 	PRIMARY KEY (labID, profID),
 	FOREIGN KEY (profID) REFERENCES Prof(profID)
-);
-
-CREATE TABLE LabArea (
-	labID INTEGER,
-	areaID INTEGER,
-	PRIMARY KEY (labID, areaID),
-	FOREIGN KEY (labID) REFERENCES Lab(labID),
-	FOREIGN KEY (areaID) REFERENCES AreaCientifica(areaID)
 );
 
 CREATE TABLE ProfAssocLab (
@@ -190,9 +126,8 @@ CREATE TABLE ComissaoAcompanhamento (
 CREATE TABLE Nucleo (
 	nucleoID INTEGER PRIMARY KEY,
 	nome Text NOT NULL,
-	salaID INTEGER,
+	sala Text UNIQUE,
 	FOREIGN KEY (nucleoID) REFERENCES Nucleo(nucleoID),
-	FOREIGN KEY (salaID) REFERENCES Sala(salaID)
 );
 
 CREATE TABLE EstudanteNucleo (
@@ -200,32 +135,6 @@ CREATE TABLE EstudanteNucleo (
 	nucleoID INTEGER,
 	PRIMARY KEY (estudanteID, nucleoID),
 	FOREIGN KEY (estudanteID) REFERENCES Estudante(estudanteID),
-	FOREIGN KEY (nucleoID) REFERENCES Nucleo(nucleoID)
-);
-
-CREATE TABLE Evento (
-	eventoID INTEGER,
-	nome Text NOT NULL,
-	dataInit DATE NOT NULL,
-	dataFim DATE NOT NULL,
-	salaID INTEGER,
-	PRIMARY KEY (eventoID, salaID),
-	FOREIGN KEY (salaID) REFERENCES Sala(salaID)
-);
-
-CREATE TABLE ProfOrganiza (
-	profID INTEGER,
-	eventoID INTEGER,
-	PRIMARY KEY (profID, eventoID),
-	FOREIGN KEY (profID) REFERENCES Prof(profID),
-	FOREIGN KEY (eventoID) REFERENCES Evento(eventoID)
-);
-
-CREATE TABLE NucleoOrganiza (
-	eventoID INTEGER,
-	nucleoID INTEGER,
-	PRIMARY KEY (eventoID, nucleoID),
-	FOREIGN KEY (eventoID) REFERENCES Evento(eventoID),
 	FOREIGN KEY (nucleoID) REFERENCES Nucleo(nucleoID)
 );
 
